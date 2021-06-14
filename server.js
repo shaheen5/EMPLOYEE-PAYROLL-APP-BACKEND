b6607d9f7dee16647d0b83a6cd2926681526d9fd
shaheen5 <miyashaheen1995@gmail.com>
2021-06-15 00:52:37 +0530

    feat : create entry point of app by setting up server

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+
+//create express app
+const app = express();
+
+//parse requests of content-type application/x-www-form-urlencoded
+app.use(express.urlencoded({extended:true}))
+
+// parse requests of content-type - application/json
+app.use(express.json());
+
+//define a simple route
+app.get('/',(req,res)=>{
+    res.json({"message" : "Welcome To Employee Payroll Application"});
+});
+
+app.listen(8000,()=>{
+    console.log("Server is listening at port number 8000");
+})
