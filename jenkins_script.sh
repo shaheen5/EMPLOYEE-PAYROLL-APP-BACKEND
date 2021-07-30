2a610c6afb5f126a13c707d4c90b302f04a192bc
shaheen5 <miyashaheen1995@gmail.com>
2021-07-30 15:38:03 +0530

    refactor : added pm2 in jenkins script

--- jenkins_script.sh
+++ jenkins_script.sh
@@ -3,6 +3,7 @@
 cd /home/ubuntu/Employee_Payroll_App_BackEnd
 working_directory=$(pwd)
 echo "Present working directory = $working_directory"
+pm2 delete 0
 npm i
 echo "Installing packages"
-npm start
+pm2 --name Employee_Payroll_App_BackEnd start npm -- start
